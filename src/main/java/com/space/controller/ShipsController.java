141dbdb9b93aff5605afcace225d942dffdc79b9
Antony.Glim <Antony.Glim@gmail.com>
2019-09-16 21:37:31 +0300

    Controller, Repository and server in process but it does not work

---
+++ src/main/java/com/space/controller/ShipsController.java
@@ -0,0 +1,33 @@
+package com.space.controller;
+
+import com.space.entities.Ship;
+import com.space.service.ShipsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/ships")
+public class ShipsController {
+
+    private ShipsService shipsService;
+
+    @Autowired
+    public void setShipsService(ShipsService shipsService) {
+        this.shipsService = shipsService;
+    }
+
+//    @GetMapping()
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<Ship> findAll(){
+//        return shipsService.findAll();
+//    }
+
+
+}
--- src/main/java/com/space/repository/ShipsRepository.java
+++ src/main/java/com/space/repository/ShipsRepository.java
@@ -1,4 +1,11 @@
 package com.space.repository;
 
-public class ShipsRepository {
+import com.space.entities.Ship;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShipsRepository extends PagingAndSortingRepository<Ship, Long>, JpaSpecificationExecutor<Ship> {
+
 }

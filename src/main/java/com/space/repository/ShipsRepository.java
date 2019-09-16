36a3edea9244336bda82d6588900a3eee8a3db2c
Antony.Glim <Antony.Glim@gmail.com>
2019-09-16 21:40:02 +0300

    Revert "Controller, Repository and server in process but it does not work"
    This reverts commit 141dbdb9b93aff5605afcace225d942dffdc79b9.

--- src/main/java/com/space/repository/ShipsRepository.java
+++ src/main/java/com/space/repository/ShipsRepository.java
@@ -1,11 +1,4 @@
 package com.space.repository;
 
-import com.space.entities.Ship;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShipsRepository extends PagingAndSortingRepository<Ship, Long>, JpaSpecificationExecutor<Ship> {
-
+public class ShipsRepository {
 }
--- src/main/java/com/space/service/ShipsService.java
+++ src/main/java/com/space/service/ShipsService.java
@@ -1,26 +1,4 @@
 package com.space.service;
 
-import com.space.entities.Ship;
-import com.space.repository.ShipsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
 public class ShipsService {
-
-    private ShipsRepository shipsRepository;
-
-    @Autowired
-    public void setShipsRepository(ShipsRepository shipsRepository) {
-        this.shipsRepository = shipsRepository;
-    }
-
-//    public List<Ship> findAll() {
-//        List<Ship> list = new ArrayList<>();
-//        shipsRepository.findAll().forEach(list::add);
-//        return list;
-//    }
 }

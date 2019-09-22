4175dfa3211a0ec458671236c412a6117975e698
Antony.Glim <Antony.Glim@gmail.com>
2019-09-22 12:22:47 +0300

    Example with products delete

--- src/main/java/com/space/repository/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.space.repository;
-
-import com.space.model.Product;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ProductsRepository extends PagingAndSortingRepository<Product, Long>, JpaSpecificationExecutor<Product> {
-}

f24b08dbe956a999cfc278fe7227880ee9cb6bf1
Antony.Glim <Antony.Glim@gmail.com>
2019-09-22 13:04:42 +0300

    RequestParam to method get add

--- src/main/java/com/space/controller/ShipsController.java
+++ src/main/java/com/space/controller/ShipsController.java
@@ -1,10 +1,11 @@
 package com.space.controller;
 
 import com.space.model.Ship;
+import com.space.model.ShipType;
 import com.space.service.ShipsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,21 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("rest/ships")
+@RequestMapping("/rest")
 public class ShipsController {
 
-    ShipsService shipsService;
+    private ShipsService shipsService;
 
     @Autowired
     public void setShipsService(ShipsService shipsService) {
         this.shipsService = shipsService;
     }
 
-    @GetMapping("/123123")
+    @GetMapping("/ships")
     @ResponseStatus(HttpStatus.OK)
-    public List<Ship> findAll(){
+    public List<Ship> findAll(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "planet", required = false) String planet,
+            @RequestParam(value = "shipType", required = false) ShipType shipType,
+            @RequestParam(value = "after", required = false) Long after,
+            @RequestParam(value = "before", required = false) Long before,
+            @RequestParam(value = "isUsed", required = false) Boolean isUsed,
+            @RequestParam(value = "minSpeed", required = false) Double minSpeed,
+            @RequestParam(value = "maxSpeed", required = false) Double maxSpeed,
+            @RequestParam(value = "minCrewSize", required = false) Integer minCrewSize,
+            @RequestParam(value = "maxCrewSize", required = false) Integer maxCrewSize,
+            @RequestParam(value = "minRating", required = false) Double minRating,
+            @RequestParam(value = "maxRating", required = false) Double maxRating,
+            @RequestParam(value = "order", required = false, defaultValue = "ID") ShipOrder order,
+            @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Integer pageNumber,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "5") Integer pageSize)
+    {
         return shipsService.findAll();
     }
+
+
 
     @GetMapping("/123123123")
     @ResponseStatus(HttpStatus.OK)
